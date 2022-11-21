d2c3ff01ed606e7dab6c6ce30556896b19394b20
yjdong9697 <yjdong9697@yonsei.ac.kr>
2022-11-21 21:39:29 +0900

    index change

--- datasets/uefa/uefa_crawling.py
+++ datasets/uefa/uefa_crawling.py
@@ -12,6 +12,7 @@ soup = BeautifulSoup(page, 'html.parser')
 t = soup.find_all(attrs= {"class" : "sc-pk-table-row-h sc-pk-table-row-s pk-table--row hydrated"})
 
 store_tmp = []
+team_store = []
 
 for idx, t in enumerate(t): 
     cnt = 0
@@ -19,9 +20,7 @@ for idx, t in enumerate(t):
     for iter in range(2, 13):
         if (iter == 2):
             team =  t.contents[iter].contents[1].contents[0].contents[1].contents[0]
-            league = t.contents[iter].contents[1].contents[0].contents[2].contents[0]
-            store_tmp.append(team)
-            store_tmp.append(league)
+            team_store.append(team)
         
         if(3 <= iter <= 12):
             value = t.contents[iter].contents[1]
@@ -33,12 +32,12 @@ for idx, t in enumerate(t):
     if(cnt != 0):
         mean = round(sum / cnt, 3)
 
-    for i in range(idx * 12 + 2, idx * 12 + 12):
+    for i in range(idx * 10, idx * 10 + 10):
         if(store_tmp[i] == "-"):
             store_tmp[i] = mean
 
-numpy_tmp = np.array(store_tmp).reshape(-1, 12)
+numpy_tmp = np.array(store_tmp).reshape(-1, 10)
 
-result = pd.DataFrame(numpy_tmp, columns = ["team", "league", "2012-2013", "2013-2014", "2014-2015", "2015-2016", "2016-2017", "2017-2018", "2018-2019", "2019-2020", "2020-2021", "2021-2022"])
+result = pd.DataFrame(numpy_tmp, columns = ["2012-2013", "2013-2014", "2014-2015", "2015-2016", "2016-2017", "2017-2018", "2018-2019", "2019-2020", "2020-2021", "2021-2022"], index = team_store)
 
-result.to_csv('./uefa/uefa_result.csv', sep = ',')
+result.to_csv('./datasets/uefa/uefa_result.csv', sep = ',')
